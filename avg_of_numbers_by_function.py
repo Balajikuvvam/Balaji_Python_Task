8829e8c8bd6d901cd6650bdd3131fb55e90b9c08
Balajikuvvam <143394998+Balajikuvvam@users.noreply.github.com>
2024-07-24 21:31:52 +0530

    Add files via upload

---
+++ avg_of_numbers_by_function.py
@@ -0,0 +1,80 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+def c(*args)
+SyntaxError: incomplete input
+
+= RESTART: C:/Users/balaj/AppData/Local/Programs/Python/Python312/Add_number_get_avg.py
+5.0
+
+= RESTART: C:/Users/balaj/AppData/Local/Programs/Python/Python312/Add_number_get_avg.py
+0.1111111111111111
+def avg(*args):
+    for i in args:
+        sum = 0
+        sum+=i
+        a=len(args)
+        avg=sum/a
+        return avg
+print(avg(1,2,3,4,5,6,7,8,9))
+SyntaxError: invalid syntax
+def avg(*args):
+    for i in args:
+        sum = 0
+        sum+=i
+        a=len(args)
+        avg=sum/a
+        return avg
+
+    
+
+
+1,2,3,4,5,6,7
+(1, 2, 3, 4, 5, 6, 7)
+
+avg=1,2,3,4,5,6,7,8,9
+
+avg
+(1, 2, 3, 4, 5, 6, 7, 8, 9)
+\
+args
+Traceback (most recent call last):
+  File "<pyshell#12>", line 1, in <module>
+    args
+NameError: name 'args' is not defined
+
+= RESTART: C:/Users/balaj/AppData/Local/Programs/Python/Python312/Add_number_get_avg.py
+Traceback (most recent call last):
+  File "C:/Users/balaj/AppData/Local/Programs/Python/Python312/Add_number_get_avg.py", line 8, in <module>
+    print(avg(1,2,3,4,5,6,7,8,9))
+NameError: name 'avg' is not defined
+
+= RESTART: C:/Users/balaj/AppData/Local/Programs/Python/Python312/Add_number_get_avg.py
+Traceback (most recent call last):
+  File "C:/Users/balaj/AppData/Local/Programs/Python/Python312/Add_number_get_avg.py", line 9, in <module>
+    print(avg(1,2,3,4,5,6,7,8,9))
+NameError: name 'avg' is not defined
+>>> 
+= RESTART: C:/Users/balaj/AppData/Local/Programs/Python/Python312/Add_number_get_avg.py
+Traceback (most recent call last):
+  File "C:/Users/balaj/AppData/Local/Programs/Python/Python312/Add_number_get_avg.py", line 9, in <module>
+    print(avg(1,2,3,4,5,6,7,8,9))
+NameError: name 'avg' is not defined
+>>> 
+= RESTART: C:/Users/balaj/AppData/Local/Programs/Python/Python312/Add_number_get_avg.py
+Traceback (most recent call last):
+  File "C:/Users/balaj/AppData/Local/Programs/Python/Python312/Add_number_get_avg.py", line 9, in <module>
+    print(avg(1,2,3,4,5,6,7,8,9))
+NameError: name 'avg' is not defined
+>>> 
+= RESTART: C:/Users/balaj/AppData/Local/Programs/Python/Python312/Add_number_get_avg.py
+9
+>>> 
+= RESTART: C:/Users/balaj/AppData/Local/Programs/Python/Python312/Add_number_get_avg.py
+45
+>>> 
+= RESTART: C:/Users/balaj/AppData/Local/Programs/Python/Python312/Add_number_get_avg.py
+5.0
+>>> 
+= RESTART: C:/Users/balaj/AppData/Local/Programs/Python/Python312/Add_number_get_avg.py
+5.0
+93.6
